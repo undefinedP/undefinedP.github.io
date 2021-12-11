73b5b46531efb77eaf0e046c389f0cff7c1a7aad
undefinedP <kzbaram@gmail.com>
2021-12-12 01:13:56 +0900

    chore: config

--- gatsby-config.js
+++ gatsby-config.js
@@ -96,8 +96,8 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-robots-txt',
       options: {
-        host: 'https://your-blog.netlify.app',
-        sitemap: 'https://your-blog.netlify.app/sitemap.xml',
+        host: 'https://undefinedp.github.io',
+        sitemap: 'https://undefinedp.github.io/sitemap.xml',
         policy: [
           {
             userAgent: '*',
